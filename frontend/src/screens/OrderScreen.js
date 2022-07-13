46005654cd315e29549f40b9023fe549c7039e47
Mithun <mithun.mani@ust.com>
2022-07-13 19:27:57 +0530

    Second Push

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -52,6 +52,7 @@ const OrderScreen = ({ match, history }) => {
     }
 
     const addPayPalScript = async () => {
+      console.log(clienId)
       const { data: clientId } = await axios.get('/api/config/paypal')
       const script = document.createElement('script')
       script.type = 'text/javascript'
@@ -74,6 +75,8 @@ const OrderScreen = ({ match, history }) => {
         setSdkReady(true)
       }
     }
+    addPayPalScript()
+    dispatch(getOrderDetails(orderId))
   }, [dispatch, orderId, successPay, successDeliver, order])
 
   const successPaymentHandler = (paymentResult) => {
