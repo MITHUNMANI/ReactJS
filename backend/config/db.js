0373c0b2f1464aed94de6c3c0f7fc77e0bc1ff2e
Mithun <mithunmani2597@gmail.com>
2023-10-30 12:31:54 +0530

    fix: db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () =>{
-
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-            // useUnifiedTopology:true,
-            // useNewUrlParser:true,
-            // UseCreateIndex:true
+        const conn = await mongoose.connect('mongodb+srv://Mithun:Mithun@mithunreact.tqt8q.mongodb.net/test?retryWrites=true&w=majority',{
+            useUnifiedTopology:true,
+            useNewUrlParser:true,
+            UseCreateIndex:true
         })
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error){
